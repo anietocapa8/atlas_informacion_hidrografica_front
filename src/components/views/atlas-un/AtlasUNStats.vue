e7e6dba9c574c4005448b95542232103a603bab4
Catalina Sarria <cimorenos@unal.edu.co>
2023-04-20 14:27:32 -0500

    Add stats component

---
+++ src/components/views/atlas-un/AtlasUNStats.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="atlas-un-stats" class="atlas-un-stats">
+    </div>
+</template>
+
+<script>
+import { loadModules } from 'esri-loader'
+export default {
+    name: 'AtlasUNStats',
+    props: ['actualAtlas'],
+    data() {
+        return {
+            map: null,
+            mapview: null,
+        };
+    },
+    mounted() {
+        if (!this.mapview) {
+            loadModules([
+                "esri/config",
+                "esri/Map",
+                "esri/views/MapView",
+                "esri/layers/FeatureLayer",
+                "esri/widgets/LayerList",
+            ]).then(([
+                esriConfig,
+                Map,
+                MapView,
+                FeatureLayer,
+                LayerList,
+            ]) => {
+
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+.atlas-un-stats {
+    width: 100%;
+    height: 100%;
+}
+</style>
