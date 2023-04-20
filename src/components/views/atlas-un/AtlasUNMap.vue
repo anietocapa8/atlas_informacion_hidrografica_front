ef70ccf7d9563dd35d5044b49ddc236468d6a7b9
Catalina Sarria <cimorenos@unal.edu.co>
2023-04-20 09:14:49 -0500

    Add layers and implement arcgis style

--- src/components/views/atlas-un/AtlasUNMap.vue
+++ src/components/views/atlas-un/AtlasUNMap.vue
@@ -14,37 +14,85 @@ export default {
     },
     mounted() {
         if (!this.mapview) {
-            loadModules(["esri/Map", "esri/views/MapView", "esri/layers/MapImageLayer"]).then(([Map, MapView, MapImageLayer]) => {
+            loadModules([
+                "esri/config",
+                "esri/Map",
+                "esri/views/MapView",
+                "esri/layers/FeatureLayer",
+                "esri/widgets/LayerList",
+            ]).then(([
+                esriConfig,
+                Map,
+                MapView,
+                FeatureLayer,
+                LayerList,
+            ]) => {
+
+                // esriConfig.apiKey = "AAPK5916cf3e3997416c8b5138a4c41e72d7L73Yx1kVNgz9TY6PRKjUUeZU4gKPJ0sthz9sl8UeoBT8dx2617lBXoV2lzPXjv2g";
+
                 const map = new Map({
                     basemap: "topo-vector"
                 });
-                const layer0 = new MapImageLayer({
-                    url: "https://serviciosgeo.sdp.gov.co/arcgis/rest/services/SIPSDER/Desarrollo_rural/MapServer/0"
-                });
-                const layer1 = new MapImageLayer({
-                    url: "https://serviciosgeo.sdp.gov.co/arcgis/rest/services/SIPSDER/Desarrollo_rural/MapServer/1"
-                });
-                map.add(layer0);
-                map.add(layer1);
+
                 const view = new MapView({
                     container: 'atlas-un-map',
                     map: map,
                 });
+
+                const base_URL = (
+                    "https://serviciosgeo.sdp.gov.co/arcgis/rest/" +
+                    "services/SIPSDER/Desarrollo_rural/MapServer/"
+                );
+
+                for (let layer = 22; layer > -1 ; layer--) {
+
+                    const featureLayer = new FeatureLayer({
+                        url: `${base_URL}${layer}`,
+                    });
+
+                    map.add(featureLayer);
+
+                    featureLayer.when(() => {
+                        console.log(
+                            "FeatureLayer fields", featureLayer.fields.map(field => {
+                                return field.alias
+                            })
+                        );
+                    });
+
+                }
+
+                let layerList = new LayerList({
+                    view: view
+                });
+
                 view.scale = 2400;
+
+                view.ui.add(
+                    layerList, {position: "top-left"}
+                );
+
                 view.when(function () {
                     // MapView is now ready for display and can be used. Here we will
                     // use goTo to view a particular location at a given zoom level and center
+
                     view.goTo({
-                        center: [-74, 4],
-                        zoom: 12
+                        center: [-74.1, 4.5],
+                        zoom: 9,
+                        rotation: -90,
                     });
+
                 })
-                    .catch(function (err) {
-                        // A rejected view indicates a fatal error making it unable to display.
-                        // Use the errback function to handle when the view doesn't load properly
-                        console.error("MapView rejected:", err);
-                    });
+                .catch(function (err) {
+                    // A rejected view indicates a fatal error making it unable to display.
+                    // Use the errback function to handle when the view doesn't load properly
+
+                    console.error("MapView rejected:", err);
+
+                });
+
                 this.map = map;
+                this.mapView = view;
             });
         }
     },
@@ -61,8 +109,9 @@ export default {
 </script>
 
 <style scoped>
+@import "https://js.arcgis.com/4.26/@arcgis/core/assets/esri/themes/dark/main.css";
 .atlas-un-map {
     width: 100%;
-    height: 100%;
+    height: 50%;
 }
 </style>
